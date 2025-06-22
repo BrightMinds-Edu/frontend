70646177d63660c7bb79e8afe952d9d4c87c616a
Leshmith <leshmith1212@gmail.com>
2025-06-22 10:01:15 +0530

    Updated.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/homePage/homePage'
 import AddQuestionPage from './pages/addQuestionPage/addQuestionPage'
 import './App.scss'
 
 const App: React.FC = () => {
-  const [message, setMessage] = useState<string>('')
+  // const [message, setMessage] = useState<string>('')
 
-  useEffect(() => {
-    // Deployed backend URL
-    fetch('https://1vtaztyw35.execute-api.eu-north-1.amazonaws.com/dev/api/data', {
-      credentials: 'include',
-    })
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-      .catch(error => console.error('Error fetching data:', error))
-  }, [])
+  // useEffect(() => {
+  //   // Deployed backend URL
+  //   fetch('https://1vtaztyw35.execute-api.eu-north-1.amazonaws.com/dev/api/data', {
+  //     credentials: 'include',
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => setMessage(data.message))
+  //     .catch(error => console.error('Error fetching data:', error))
+  // }, [])
 
   return (
     <Router>
