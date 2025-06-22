4c87fb5a8216c451168755defec4f05a505c8026
Leshmith <leshmith1212@gmail.com>
2025-06-22 11:11:49 +0530

    Updated css for buttons in addQuestionPage.

--- src/pages/addQuestionPage/addQuestionPage.tsx
+++ src/pages/addQuestionPage/addQuestionPage.tsx
@@ -2,6 +2,8 @@ import React, { useMemo, useState } from 'react';
 import { v4 as uuid } from 'uuid';
 import AddQuestionCard, { QuestionData } from '../../components/addQuestionCard/addQuestionCard';
 import './addQuestionPage.scss';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const AddQuestionPage: React.FC = () => {
   const [questions, setQuestions] = useState<QuestionData[]>([]);
@@ -36,10 +38,26 @@ const AddQuestionPage: React.FC = () => {
     setSelectedId(null);
   };
 
-  const addNew = () => setSelectedId(null);
+  const addNew = () => {
+    const newQuestion: QuestionData = {
+      id: uuid(),
+      question: '',
+      options: ['', '', ''],
+    };
+    setQuestions((prev) => [...prev, newQuestion]);
+    setSelectedId(newQuestion.id);
+  };
+
+  const addToQuestionBank = () => {
+    toast.info('Processing...', { autoClose: 2000 });
+    setTimeout(() => {
+      toast.success('Successfully Added to Question Bank!', { autoClose: 2000 });
+    }, 2000);
+  };
 
   return (
     <div className="add-question-page">
+      <ToastContainer />
       {/* ► Side panel */}
       <aside className="sidebar">
         <h3>Questions</h3>
@@ -57,10 +75,13 @@ const AddQuestionPage: React.FC = () => {
               {`Q${idx + 1}: ${q.question.slice(0, 30) || '(untitled)'}`}
             </li>
           ))}
+          <li className="add-btn" onClick={addNew}>
+            + New question
+          </li>
         </ul>
 
-        <button className="btn primary add-btn" onClick={addNew}>
-          + New question
+        <button className="btn primary" onClick={addToQuestionBank}>
+          Add to Question Bank
         </button>
       </aside>
 
